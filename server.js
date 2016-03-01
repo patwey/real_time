032802dd1b56948d8bf83c8ead4cddbf79dd6c77
Pat Wey <patrickwey@gmail.com>
2016-03-01 15:33:35 -0700

    Switch to jade for views

--- server.js
+++ server.js
@@ -10,9 +10,19 @@ app.use(express.static('static'));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Real Time';
 
+app.set('view engine', 'jade');
+
+// 'controller' stuff
+
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/static/index.html'));
+  response.render('index')
 });
+
+app.get('/new', (request, response) => {
+  response.render('new')
+});
+
+// 'socket' stuff
 
 io.on('connection', (socket) => {
   socket.emit('message', { text: 'Hello world' });
